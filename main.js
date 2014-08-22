26de2df798540f92968067c2136c19181235457f
cas <casska@gmail.com>
2014-08-22 15:33:50 -0500

    better code

--- main.js
+++ main.js
@@ -1,60 +1,77 @@
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-var canvas, mask;
-
+/*global window */
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
-window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-                            window.webkitRequestAnimationFrame || window.oRequestAnimationFrame;
-var source;
-var buffer;
-var audioBuffer;
-var dropArea;
-var audioContext;
-var processor;
-var analyser;
-var sampleAudioURL = 'owm.mp3';
-var isPlayingAudio = false;
+window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.oRequestAnimationFrame;
+
 
 // analysis variables
 var waveData = []; //waveform - from 0 - 1 . no sound is 0.5. Array [binCount]
-var levelsCount = 4; //should be factor of 512
-var levelHistory = []; //last 256 ave norm levels
-var volSens = 1;
+//var levelsCount = 4; //should be factor of 512
 var past_zoom = 0;
 
-var mask_size;
+var Outworld = {
 
-var audio = {
-  init: function() {
-    audioContext = new AudioContext();
+  // Settings
+  sampleAudioURL: 'owm.mp3',
+  isPlayingAudio: false,
+  levelsCount: 4,
+  volSens: 0.9,
+  mask: '',
+  analyser: '',
 
+  // Init
+  init: function () {
+    
+    // Create an empty project and a view for the canvas:
+    paper.setup(document.getElementById('myCanvas'));
+
+    // Create raster for mask
+    mask = new paper.Raster('mask');
+
+    // Move the raster to the center of the view
+    mask.position = paper.view.center;
+
+    // Scale mask to fit canvas
+    mask.scale((paper.view.size.height * 0.88) / mask.height);
+    
+    // Redraw Canvas
+    paper.view.draw();
+
+    // Create audio context
+    var audioContext = new AudioContext();
+
+    // Create analyser node
     analyser = audioContext.createAnalyser();
-    analyser.smoothingTimeConstant = 0.8; //0<->1. 0 is no time smoothing
-    analyser.fftSize = 64;
-    analyser.connect(audioContext.destination);
-    binCount = analyser.frequencyBinCount; // = 512
-    levelBins = Math.floor(binCount / levelsCount); //number of bins in each level
-    freqByteData = new Uint8Array(binCount);
-    timeByteData = new Uint8Array(binCount);
 
-    // init sound
-    source = audioContext.createBufferSource();
+    // Set analyser parameteres
+    analyser.smoothingTimeConstant = 0.8;
+    analyser.fftSize = 64;
+
+    // Connect analyser -> audio context
+    analyser.connect(audioContext.destination);
+
+
+    binCount = analyser.frequencyBinCount; // = 512
+    levelBins = Math.floor(binCount / this.levelsCount); //number of bins in each level
+
+    // Create buffer for song
+    var source = audioContext.createBufferSource();
+
+    // Connect source -> analyser
     source.connect(analyser);
 
-    // Load asynchronously
+    // Load file asynchronously
     var request = new XMLHttpRequest();
-    request.open('GET', sampleAudioURL, true);
+    request.open('GET', this.sampleAudioURL, true);
     request.responseType = 'arraybuffer';
 
-
     request.onload = function() {
+      var buffer;
       audioContext.decodeAudioData(request.response, function(buffer) {
 
         source.buffer = buffer;
         source.start(0);
-        isPlayingAudio = true;
+        this.isPlayingAudio = true;
+        Outworld.render();
 
       }, function(e) {
         console.log(e);
@@ -63,36 +80,49 @@ var audio = {
 
     request.send();
   },
-  analyse: function() {
-    //analyser.getByteTimeDomainData(timeByteData);
-    analyser.getByteFrequencyData(timeByteData);
 
-    for (var i = 0; i < binCount; i++) {
-      waveData[i] = ((timeByteData[i] - 128) / 128) * volSens;
-    }
+  analyse: function () {
+    
+    var freqByteData = new Uint8Array(binCount);
+    
+    // Get freq analisys data
+    analyser.getByteFrequencyData(freqByteData);
+
+    // Copy freq data to waveData, modified by volSens
+    for (var i = 0; i < binCount; i++)
+      waveData[i] = ((freqByteData[i] - 128) / 128) * this.volSens;
+
   },
-  render: function() {
 
-    window.requestAnimationFrame(audio.render);
+  render: function () {
 
-    if (isPlayingAudio) {
+    // Request animation frame
+    window.requestAnimationFrame(Outworld.render);
 
-      audio.analyse();
+    // if audio is playing
+    if (this.isPlayingAudio) {
 
-      var shiftx = waveData[getRandomInt(50, 60)];
-      var shifty = waveData[getRandomInt(410, 470)];
+      // Get analyser data
+      Outworld.analyse();
 
-      var zoom = waveData[0] * 0.6;
+      // Set zoom level
+      var zoom = waveData[0];
+
+      // Set hue level
       var hue = waveData[3] * 360;
        
-      //var mask = $('#mask');
+      // if Mask exist
       if (mask) {
+
+        // if difference with past zoom state is ± 0.02
         if (zoom - past_zoom > 0.02 || zoom - past_zoom < -0.02) {
+
+          // Apply zoom
           mask.scale( ( paper.view.size.height * zoom ) / mask.bounds.height );
           
+          // Save new zoom state for future reference
           past_zoom = zoom;
-          var opacity = (zoom * 9) - 1;
-          //mask.css('transform','scale(' + zoom + ',' + zoom + ')').css('opacity', opacity);
+
         }
 
         if($('#bg').css('-webkit-filter') )
@@ -112,79 +142,32 @@ var audio = {
           
         $('#test-bar').css('width', zoom * 100  + 'px');
 
-/*
-        if (shiftx > 0.005) {
-          mesh.rotation.x += shiftx;
-        } else {
-          mesh.rotation.x += 0.005;
-        }
-
-        if (shifty > 0.005) {
-          mesh.rotation.y += shifty;
-        } else {
-          mesh.rotation.y += 0.005;
-        }
-
-        if (camera.position.z < 3.6) {
-          camera.position.z = (camera.position.z * (zoom + 1.75));
-        }
-
-        camera.position.z = (camera.position.z * (zoom + 1));
-        */
         paper.view.draw();
       }
 
     } else {
-/*
-      if (mesh) {
-
-        mesh.rotation.x += 0.005;
-        mesh.rotation.y += 0.005;
-
-      }
-*/
 
     }
+  },
+  resize: function () {
+ 
+    // Move the raster to the center of the view
+    mask.position = paper.view.center;
+
+    // Scale mask to fit canvas
+    mask.scale((paper.view.size.height * 0.88) / mask.height);
+    
+
+    // Redraw Canvas
+    paper.view.draw();
+
   }
 };
 
-window.onload = function() {
-  // Get a reference to the canvas object
-  canvas = document.getElementById('myCanvas');
-  // Create an empty project and a view for the canvas:
-  paper.setup(canvas);
-
-
-  // Create raster from bg
-  mask = new paper.Raster('mask');
-
-  // Move the raster to the center of the view
-  mask.position = paper.view.center;
-
-  // Scale mask to fit canvas
-  mask.scale((paper.view.size.height * 0.88) / mask.height);
-  
-
-  // Redraw Canvas
-  paper.view.draw();
-  
-  audio.init();
-  audio.render();
-
+window.onload = function() { 
+  Outworld.init();
 }
 
 window.onresize = function(event) {
-  
-  // Move the raster to the center of the view
-  mask.position = paper.view.center;
-
-  // Scale mask to fit canvas
-  mask.scale((paper.view.size.height * 0.88) / mask.height);
-  
-
-  // Redraw Canvas
-  paper.view.draw();
-
+  Outworld.resize();
 };
-
-
